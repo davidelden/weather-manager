cf3805e8239785eb2d3db8b517f352bd9b2f749c
davidelden <deelden+github@gmail.com>
2019-11-12 17:38:58 -0600

    add api baseURL, subscriber routes to index.js

--- src/api/server.js
+++ src/api/server.js
@@ -1,6 +1,11 @@
 const express = require('express'),
       app = express(),
-      port = process.env.PORT || 3000;
+      port = process.env.PORT || 3000,
+      subscribers = require('./routes/subscribers.js');
+
+app.locals.baseURL = '/api/v1/weather-manager';
+
+app.use(`${app.locals.baseURL}/subscribers`, subscribers);
 
 const startServer = () => {
   app.listen(port, () => console.log(`Example app listening on port ${port}!`));
