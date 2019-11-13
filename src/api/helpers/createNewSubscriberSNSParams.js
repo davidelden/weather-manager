1db79a3c99046fce74ec05d46cb7f55927d60e2a
davidelden <deelden+github@gmail.com>
2019-11-13 15:51:18 -0600

    add createNewSubscriberSNSParams fn

---
+++ src/api/helpers/createNewSubscriberSNSParams.js
@@ -0,0 +1,13 @@
+const createNewSubscriberSNSParams = (phone_number, zip_codes) => (
+  {
+    "Protocol": "sms",
+    "TopicArn": process.env.AWS_SNS_TOPIC_ARN,
+    "Attributes": {
+      "FilterPolicy": `{ "zip_codes": [${zip_codes}] }`
+    },
+    "Endpoint": phone_number,
+    "ReturnSubscriptionArn": true
+  }
+)
+
+module.exports = createNewSubscriberSNSParams;
