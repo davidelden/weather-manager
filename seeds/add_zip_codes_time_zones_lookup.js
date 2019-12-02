8b85e9878c8ed7455efc8891b32b8d5ee24eea00
davidelden <deelden+github@gmail.com>
2019-12-02 11:10:35 -0600

    add knex seed script, local seed file

---
+++ seeds/add_zip_codes_time_zones_lookup.js
@@ -0,0 +1,11 @@
+const csvFilePath = '/var/lib/postgresql/seeds/zip_codes_time_zones_lookup.csv';
+
+exports.seed = function(knex) {
+  // Deletes ALL existing entries
+  return knex('zip_codes_time_zones_lookup').del()
+    .then(function () {
+      // Inserts seed entries
+      // return knex('zip_codes_time_zones_lookup').insert(zipCodesTimeZones);
+      return knex.raw(`COPY zip_codes_time_zones_lookup FROM '${csvFilePath}' DELIMITER ',' CSV HEADER`)
+    });
+};
