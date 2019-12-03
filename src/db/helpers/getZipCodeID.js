261ddf34c18e2d6f232f7dfff71f56bdb2a52c13
davidelden <deelden+github@gmail.com>
2019-12-03 13:08:59 -0600

    return 0 if no data

--- src/db/helpers/getZipCodeID.js
+++ src/db/helpers/getZipCodeID.js
@@ -4,10 +4,17 @@ const getZipCodeID = zipCode => (
   db.select('id')
   .from('zip_codes')
   .where({ zip_code: zipCode })
-  .then(data => data[0].id)
+  .then(data => {
+    if(data && data.length > 0) {
+      const { id } = data[0];
+      if(id) return id;
+    }
+
+    return 0;
+  })
   .catch(err => {
     console.error(err);
-  });
+  })
 )
 
 module.exports = getZipCodeID;
