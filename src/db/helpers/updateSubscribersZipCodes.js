c92715b3a62f36ef3eba3cacb33280530eaea326
davidelden <deelden+github@gmail.com>
2019-12-06 13:33:18 -0600

    begin building updateSubscribersZipCodes helper fn

---
+++ src/db/helpers/updateSubscribersZipCodes.js
@@ -0,0 +1,26 @@
+const getZipCodesBySubscriberPhoneNumber = require('./getZipCodesBySubscriberPhoneNumber.js'),
+      getZipCodesToRemove = require('./getZipCodesToRemove.js');
+
+const updateSubscribersZipCodes = async (phoneNumber, zipCodes) => {
+  // zipCodes = ['78745', '33156', '01075']
+  const currentZipCodes = await getZipCodesBySubscriberPhoneNumber(phoneNumber),
+        removeZipCodes = getZipCodesToRemove(currentZipCodes, zipCodes);
+
+  // Compare the zipCodes array with the currentZipCodes array, which contains all zipCodes belonging to a subscriber
+  //    - Are there any to remove?
+  //      • Of the ones to remove will its removal result in it belonging to no other subscribers?
+  //    - Are there any to add?
+  //      • Of the ones to add are any new to the db table (i.e. weather data is not currently being fetched for them)?
+
+  if(removeZipCodes.length > 0) {
+    // Check if the removal of the zipCode from the subscriber results in an orphan
+    // (i.e. the zipCode will belong to no subscriber)
+
+    // if orphan >>> remove zipCode from zip_codes table && publish message
+
+  }
+
+  return currentZipCodes;
+}
+
+module.exports = updateSubscribersZipCodes;
