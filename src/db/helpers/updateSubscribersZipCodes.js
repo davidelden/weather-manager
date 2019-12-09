f6494c7c5ec2575c3cbd8f97a66e0c0b9aed89b2
davidelden <deelden+github@gmail.com>
2019-12-09 11:24:08 -0600

    add getZipCodesToAdd fn to updateSubscribersZipCodes

--- src/db/helpers/updateSubscribersZipCodes.js
+++ src/db/helpers/updateSubscribersZipCodes.js
@@ -1,9 +1,11 @@
 const getZipCodesBySubscriberPhoneNumber = require('./getZipCodesBySubscriberPhoneNumber.js'),
+      getZipCodesToAdd = require('./getZipCodesToAdd.js'),
       getZipCodesToRemove = require('./getZipCodesToRemove.js');
 
 const updateSubscribersZipCodes = async (phoneNumber, zipCodes) => {
   // zipCodes = ['78745', '33156', '01075']
   const currentZipCodes = await getZipCodesBySubscriberPhoneNumber(phoneNumber),
+        addZipCodes = getZipCodesToAdd(currentZipCodes, zipCodes),
         removeZipCodes = getZipCodesToRemove(currentZipCodes, zipCodes);
 
   // Compare the zipCodes array with the currentZipCodes array, which contains all zipCodes belonging to a subscriber
@@ -11,6 +13,13 @@ const updateSubscribersZipCodes = async (phoneNumber, zipCodes) => {
   //      • Of the ones to remove will its removal result in it belonging to no other subscribers?
   //    - Are there any to add?
   //      • Of the ones to add are any new to the db table (i.e. weather data is not currently being fetched for them)?
+
+  if(addZipCodes.length > 0) {
+    // Check if the additional zipCode to add to the subscriber is active
+    // (i.e. weather is currently fetched for the zipCode)
+
+    // If zipCode is not active add it to the zip_codes table && publish message
+  }
 
   if(removeZipCodes.length > 0) {
     // Check if the removal of the zipCode from the subscriber results in an orphan
