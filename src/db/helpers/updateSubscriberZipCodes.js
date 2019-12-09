e1772452859297da2ba58da806c2335b971273f4
davidelden <deelden+github@gmail.com>
2019-12-09 12:33:15 -0600

    add getSubscriberID, attachZipCodesToSubscriber funcs to updateSubscriberZipCodes

--- src/db/helpers/updateSubscriberZipCodes.js
+++ src/db/helpers/updateSubscriberZipCodes.js
@@ -1,24 +1,21 @@
 const getZipCodesBySubscriberPhoneNumber = require('./getZipCodesBySubscriberPhoneNumber.js'),
+      getSubscriberID = require('./getSubscriberID.js'),
       getZipCodesToAdd = require('./getZipCodesToAdd.js'),
-      getZipCodesToRemove = require('./getZipCodesToRemove.js');
+      getZipCodesToRemove = require('./getZipCodesToRemove.js'),
+      attachZipCodesToSubscriber = require('./attachZipCodesToSubscriber.js');
 
 const updateSubscriberZipCodes = async (phoneNumber, zipCodes) => {
   // zipCodes = ['78745', '33156', '01075']
+  const subscriberID = await getSubscriberID(phoneNumber);
+
+  if(subscriberID === 0) return { message: 'Subscriber not found.' };
+
   const currentZipCodes = await getZipCodesBySubscriberPhoneNumber(phoneNumber),
         addZipCodes = getZipCodesToAdd(currentZipCodes, zipCodes),
         removeZipCodes = getZipCodesToRemove(currentZipCodes, zipCodes);
 
-  // Compare the zipCodes array with the currentZipCodes array, which contains all zipCodes belonging to a subscriber
-  //    - Are there any to remove?
-  //      • Of the ones to remove will its removal result in it belonging to no other subscribers?
-  //    - Are there any to add?
-  //      • Of the ones to add are any new to the db table (i.e. weather data is not currently being fetched for them)?
-
   if(addZipCodes.length > 0) {
-    // Check if the additional zipCode to add to the subscriber is active
-    // (i.e. weather is currently fetched for the zipCode)
-
-    // If zipCode is not active add it to the zip_codes table && publish message
+    attachZipCodesToSubscriber(subscriberID, addZipCodes); // This is async
   }
 
   if(removeZipCodes.length > 0) {
