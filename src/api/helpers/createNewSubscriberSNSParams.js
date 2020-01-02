51993eb06d77cc64ed836356e69784ba61ed110e
davidelden <deelden+github@gmail.com>
2020-01-02 11:46:09 -0600

    update template literal to preserve quotation marks of strings in arr

--- src/api/helpers/createNewSubscriberSNSParams.js
+++ src/api/helpers/createNewSubscriberSNSParams.js
@@ -3,7 +3,7 @@ const createNewSubscriberSNSParams = (phone_number, zip_codes) => (
     "Protocol": "sms",
     "TopicArn": process.env.AWS_SNS_TOPIC_ARN,
     "Attributes": {
-      "FilterPolicy": `{ "zip_codes": [${zip_codes}] }`
+      "FilterPolicy": `{ "zip_codes": [\"${zip_codes.join("\",\"")}\"] }`
     },
     "Endpoint": phone_number,
     "ReturnSubscriptionArn": true
